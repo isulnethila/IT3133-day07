19147a35e8c4f0e068d398cf97d10c2d18d37e87
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-11-22 21:37:10 +0530

    Update Childcom

--- day07/src/Childcom.js
+++ day07/src/Childcom.js
@@ -2,6 +2,9 @@ export default function Childcom(props){
     return(
         <>
         <h3>Hello React I'm Child</h3>
+        {
+            props.addName("My Name is Bod")
+        }
         </>
     );
 }
