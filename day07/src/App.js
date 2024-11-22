e5f89169e37f5dec1fb05096da7fcf2022280b67
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-11-22 21:30:12 +0530

    Update App.js

--- day07/src/App.js
+++ day07/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
-
+import Childcom from './Childcom';
+import {useState} from 'react';
 function App() {
+  const[childname,setchildname]=useState("Default");
   return (
+    <div>
+      <h2>Hello React I'm Parent</h2>
+      <Childcom/>
+      <p>parent:My chlid said:{childname}</p>
+    </div>
    
   );
 }
