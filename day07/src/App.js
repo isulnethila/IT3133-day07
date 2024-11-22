4e514e7a94ce710538e75ddbd3eb02f10b510c0d
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-11-22 21:41:15 +0530

    edit app.js

--- day07/src/App.js
+++ day07/src/App.js
@@ -3,11 +3,14 @@ import Childcom from './Childcom';
 import {useState} from 'react';
 function App() {
   const[childname,setchildname]=useState("Default");
+  const receiveName=(name)=>{
+    setchildname(name);
+  }
   return (
     <div>
       <h2>Hello React I'm Parent</h2>
-      <Childcom/>
       <p>parent:My chlid said:{childname}</p>
+      <Childcom addName={receiveName}/>
     </div>
    
   );
